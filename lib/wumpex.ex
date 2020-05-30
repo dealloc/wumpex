559a42ae98fd50488e4570a2aeb193d92dc88502
Wannes Gennar <wannes.gennar@gmail.com>
2020-05-30 11:00:51 +0200

    add Wumpex.token/0
    Added method for retrieving the bot key from configuration

--- lib/wumpex.ex
+++ lib/wumpex.ex
@@ -4,15 +4,16 @@ defmodule Wumpex do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Wumpex.hello()
-      :world
-
+  Fetch the bot key from configuration.
   """
-  def hello do
-    :world
+  @spec token() :: String.t() | nil
+  def token do
+    case Application.get_env(:wumpex, :key) do
+      token when is_nil(token) or is_binary(token) ->
+        token
+
+      _token ->
+        raise "Invalid key configured!"
+    end
   end
 end
--- test/wumpex_test.exs
+++ test/wumpex_test.exs
@@ -2,7 +2,31 @@ defmodule WumpexTest do
   use ExUnit.Case
   doctest Wumpex
 
-  test "greets the world" do
-    assert Wumpex.hello() == :world
+  describe "Wumpex.token/0 should" do
+    test "return the configured token when it's a string" do
+      Application.put_env(:wumpex, :key, "some-string")
+
+      assert Wumpex.token() == "some-string"
+    end
+
+    test "return the configured token when it's nil" do
+      Application.put_env(:wumpex, :key, nil)
+
+      assert Wumpex.token() == nil
+    end
+
+    test "return nil when the token is not configured" do
+      Application.delete_env(:wumpex, :key)
+
+      assert Wumpex.token() == nil
+    end
+
+    test "raise when the configured token is not nil or a string" do
+      Application.put_env(:wumpex, :key, [])
+
+      assert_raise RuntimeError, fn ->
+        Wumpex.token()
+      end
+    end
   end
 end
