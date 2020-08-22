4bc742f7e808364968c05f46a2a79e77055c806b
Wannes Gennar <wannes.gennar@gmail.com>
2020-08-22 11:47:25 +0200

    stabilize unit tests
    Fix race conditions in the Websocket unit tests by making it wait until the test server is ready

--- test/base/websocket_test.exs
+++ test/base/websocket_test.exs
@@ -4,28 +4,36 @@ defmodule Wumpex.Base.WebsocketTest do
   @moduletag :integration
   doctest Wumpex.Base.Websocket
 
+  alias Wumpex.Base.Websocket
+
   describe "Wumpex.Base.Websocket should" do
     test "connect to websocket servers" do
-      server = WebsocketHelpers.accept_one()
-      {:ok, client} = Wumpex.Base.Websocket.start_link(url: "ws://localhost:8080", worker: self())
+      server = WebsocketHelpers.accept_one(self())
+      WebsocketHelpers.wait_for_server()
+
+      {:ok, client} = Websocket.start_link(url: "ws://localhost:8080", worker: self())
       assert Process.alive?(client)
 
       Task.await(server, 1_000)
     end
 
     test "send messages to it's worker in :text mode" do
-      server = WebsocketHelpers.echo_one()
-      {:ok, client} = Wumpex.Base.Websocket.start_link(url: "ws://localhost:8080", worker: self())
-      Wumpex.Base.Websocket.send(client, "Hello world", mode: :text)
+      server = WebsocketHelpers.echo_one(self())
+      WebsocketHelpers.wait_for_server()
+
+      {:ok, client} = Websocket.start_link(url: "ws://localhost:8080", worker: self())
+      Websocket.send(client, "Hello world", mode: :text)
       assert_receive {:"$gen_cast", {{:text, "Hello world"}, _pid}}
 
       Task.await(server, 1_000)
     end
 
     test "send messages to it's worker in :binary mode" do
-      server = WebsocketHelpers.echo_one()
-      {:ok, client} = Wumpex.Base.Websocket.start_link(url: "ws://localhost:8080", worker: self())
-      Wumpex.Base.Websocket.send(client, "Hello world", mode: :binary)
+      server = WebsocketHelpers.echo_one(self())
+      WebsocketHelpers.wait_for_server()
+
+      {:ok, client} = Websocket.start_link(url: "ws://localhost:8080", worker: self())
+      Websocket.send(client, "Hello world", mode: :binary)
       assert_receive {:"$gen_cast", {{:binary, "Hello world"}, _pid}}
 
       Task.await(server, 1_000)
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,26 +1,44 @@
 ExUnit.start(capture_log: true)
 
 defmodule WebsocketHelpers do
-  def accept_one,
-    do:
-      Task.async(fn ->
-        server = Socket.Web.listen!(8080)
-        client = Socket.Web.accept!(server)
-        Socket.Web.accept!(client)
+  @doc """
+  Accepts a single websocket connection and closes after 100ms.
+  """
+  def accept_one(client) do
+    Task.async(fn ->
+      server = Socket.Web.listen!(8080)
+      send(client, :ready) # Let the client know the server is ready to accept.
+      client = Socket.Web.accept!(server)
+      Socket.Web.accept!(client)
 
-        Process.sleep(100)
-      end)
+      Process.sleep(100)
+    end)
+  end
 
-  def echo_one,
-    do:
-      Task.async(fn ->
-        server = Socket.Web.listen!(8080)
-        client = Socket.Web.accept!(server)
-        Socket.Web.accept!(client)
+  @doc """
+  Accepts a single websocket connection, echos back the first message and closes after 100ms.
+  """
+  def echo_one(client) do
+    Task.async(fn ->
+      server = Socket.Web.listen!(8080)
+      send(client, :ready) # Let the client know the server is ready to accept.
+      client = Socket.Web.accept!(server)
+      Socket.Web.accept!(client)
 
-        message = Socket.Web.recv!(client)
-        Socket.Web.send!(client, message)
+      message = Socket.Web.recv!(client)
+      Socket.Web.send!(client, message)
 
-        Process.sleep(100)
-      end)
+      Process.sleep(100)
+    end)
+  end
+
+  @doc """
+  Wait for the websocket server (see above methods) to indicate being ready to accept connections.
+  """
+  def wait_for_server() do
+    require ExUnit.Assertions
+    import ExUnit.Assertions
+
+    assert_receive :ready
+  end
 end
