6f5595943066418c44febb8c09e723129f1f4324
Wannes Gennar <wannes.gennar@gmail.com>
2020-10-25 15:54:50 +0100

    added Wumpex.Bot
    Added Wumpex.Bot to generate handler configuration

--- .iex.exs
+++ .iex.exs
@@ -1,21 +1,25 @@
 alias Wumpex.Api
 
 # Below is some code to locally run some tests on Wumpex
+defmodule DummyBot do
+  use Wumpex.Bot
+
+  event DummyHandler
+end
+
 defmodule DummyHandler do
+  @behaviour Wumpex.Bot
+
   require Logger
 
-  def start_wumpex do
-    Wumpex.Sharding.start_link([
-      handlers: [[handler: __MODULE__]]
-    ])
-  end
-
+  @impl Wumpex.Bot
   def init do
     Logger.info("DummyHandler.init/0")
 
     nil
   end
 
+  @impl Wumpex.Bot
   def handle(event, state) do
     Logger.debug("DummyHandler: #{inspect(event)}")
 
